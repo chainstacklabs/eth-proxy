f307bfcc82bc82652e2ab53f27c5845c2b5e56a6
ezequiel <eze@blockwallet.io>
2022-03-15 17:56:48 -0300

    add failover through upstream

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,21 @@ set -eu
 export RPC_PORT="${RPC_PORT:-8545}"
 export WS_PORT="${WS_PORT:-8546}"
 
-if [[ -z "${USERNAME:-}" || -z "${USERNAME:-}" ]]; then
+if [[ ! -z "${ENDPOINT:-}"  ]]; then
+    export RPC_ENDPOINT="${ENDPOINT}"
+    export WS_ENDPOINT="${ENDPOINT}"
+fi
+
+if [[ ! -z "${ENDPOINT_FAILOVER:-}"  ]]; then
+    export RPC_ENDPOINT_FAILOVER="${ENDPOINT_FAILOVER}"
+    export WS_ENDPOINT_FAILOVER="${ENDPOINT_FAILOVER}"
+fi
+
+if [[ -z "${USERNAME:-}" || -z "${PASSWORD:-}" ]]; then
     envsubst '${RPC_PORT} ${RPC_ENDPOINT} ${WS_PORT} ${WS_ENDPOINT}' < /etc/nginx/conf.d/non-auth.conf.template > /etc/nginx/conf.d/default.conf
 else
     export AUTHORIZATION=$(echo -n "${USERNAME}:${PASSWORD}" | base64 | tr -d \\n)
-    envsubst '${RPC_PORT} ${RPC_ENDPOINT} ${WS_PORT} ${WS_ENDPOINT} ${AUTHORIZATION}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+    envsubst '${RPC_PORT} ${WS_PORT} ${RPC_ENDPOINT} ${WS_ENDPOINT} ${RPC_ENDPOINT_FAILOVER} ${WS_ENDPOINT_FAILOVER} ${AUTHORIZATION}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 fi
 
 exec "$@"
